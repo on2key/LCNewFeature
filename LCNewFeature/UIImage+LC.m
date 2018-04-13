0668c09af10cff6057ec555bfbe8c500a09fbcd8
on2key <38313077+on2key@users.noreply.github.com>
2018-04-13 11:02:14 +0800

    add iphonex fix

--- LCNewFeature/UIImage+LC.m
+++ LCNewFeature/UIImage+LC.m
@@ -12,6 +12,7 @@
 #define IPHONE5 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size) : NO)
 #define IPHONE6 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? (CGSizeEqualToSize(CGSizeMake(750, 1334), [[UIScreen mainScreen] currentMode].size) || CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size)) : NO)
 #define IPHONE6P ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? (CGSizeEqualToSize(CGSizeMake(1242, 2208), [[UIScreen mainScreen] currentMode].size) || CGSizeEqualToSize(CGSizeMake(1125, 2001), [[UIScreen mainScreen] currentMode].size)) : NO)
+#define IPHONEX ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? (CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) || CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size)) : NO)
 
 #ifdef DEBUG
 #define LCLog(...) NSLog(@"%s -> Line:%d -> %@", __func__, __LINE__, [NSString stringWithFormat:__VA_ARGS__])
@@ -42,6 +43,33 @@
     }
     
 //    LCLog(@"\nImageName: %@", realImageName);
+    
+    return [self imageNamed:realImageName];
+}
++ (instancetype)imageNamedForAdaptation:(NSString *)imageName
+                                iphone5:(BOOL)iphone5
+                                iphone6:(BOOL)iphone6
+                               iphone6p:(BOOL)iphone6p
+                               iphoneX:(BOOL)iphoneX{
+    
+    NSString *realImageName = imageName;
+    
+    if (IPHONE5 && iphone5) {           // 当前设备是 iPhone 5 / 5s 或 iPhone 6 / 6s 放大模式
+        
+        realImageName = [NSString stringWithFormat:@"%@_iphone5", realImageName];
+        
+    } else if (IPHONE6 && iphone6) {    // 当前设备是 iPhone 6 / 6s
+        
+        realImageName = [NSString stringWithFormat:@"%@_iphone6", realImageName];
+        
+    } else if (IPHONE6P && iphone6p) {  // 当前设备是 iPhone 6 p / 6s p
+        
+        realImageName = [NSString stringWithFormat:@"%@_iphone6p", realImageName];
+    }else if (IPHONEX && iphoneX) {
+        realImageName = [NSString stringWithFormat:@"%@_iphoneX", realImageName];
+    }
+    
+    //    LCLog(@"\nImageName: %@", realImageName);
     
     return [self imageNamed:realImageName];
 }
